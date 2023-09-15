473225922ee453d827df3bda4f239fc5c82ac389
Abhijit Verma <31704373+Abhivera@users.noreply.github.com>
2023-09-15 15:33:50 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 3000;
 
-export const mongoDBURL='mongodb+srv://abhi:abhiveraA1@bookstore-db.mpr5eyk.mongodb.net/?retryWrites=true&w=majority'
+export const mongoDBURL='mongodb+srv://abhijit:abhijitmern@abhijit.biqkzdj.mongodb.net/?retryWrites=true&w=majority'
